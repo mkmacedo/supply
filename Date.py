0b984268e6610c4286b509406503068bfa04f4a5
mkmacedo <markelp@protonmail.com>
2022-03-28 17:28:19 -0300

    supply plus

--- Date.py
+++ Date.py
@@ -1,6 +1,6 @@
 from datetime import date, datetime
 
-a = date.today()
+a = date.today().strftime('%B')
 print(a)
 
 datetime_obj = datetime.strptime('2020-02-02','%Y-%m-%d')
@@ -12,3 +12,6 @@ print(datetime_obj)
 print('122.2'.replace('.', '').replace(',', '').isdigit())
 
 print('1'.isdigit())
+
+
+print(a)
--- ReadExcel.py
+++ ReadExcel.py
@@ -38,7 +38,6 @@ class Medicamentos:
         self.df_estoque_all = pd.read_excel(sheets[3].strip())
         self.df_forecast = pd.read_excel(sheets[4].strip())
         self.df_colocado = pd.read_excel(sheets[5].strip())
-        print(self.df_colocado.head())
         self.df_biotech = pd.read_excel(sheets[6].strip())
 
         self.d = {}
@@ -58,7 +57,7 @@ class Medicamentos:
 
             for i in range(len(self.df_vendas)):
                 if str(self.df_vendas.loc[i, 'Material']) == f:
-                    self.d[f]['Sales'] += self.df_vendas.loc[i, 'Quantity']
+                    self.d[f]['Sales'] += self.df_vendas.loc[i, 'Quantity'] * (-1)
 
             for i in range(len(self.df_colocado)):
                 if str(self.df_colocado.loc[i, 'Código']) == f:# and str(self.df_colocado.loc[i, 'Código']).replace('.','').replace(',','').isdigit():
@@ -67,6 +66,9 @@ class Medicamentos:
             for key in list(self.d.keys()):
                 try:
                     self.d[key]['Delivery'] = self.d[f]['Colocado'] - self.d[f]['Sales']
+                    #print('Try SALES',self.d[f]['Sales'])
+                    #print('Try COLOCADO',self.d[f]['Colocado'])
+                    #print('Try DELIVERY', self.d[key]['Delivery'])
                 except:
                     pass
             
@@ -99,49 +101,22 @@ class Medicamentos:
 
                     limit = self.d[f]['Batch'][str(self.df_estoque_all.loc[i, 'Batch'])]['Shelf life'][0].date() - timedelta(days=30*12)
                     self.d[f]['Batch'][str(self.df_estoque_all.loc[i, 'Batch'])]['Limit sales date'] = (limit, limit.strftime('%Y-%m-%d'))
-                    
 
-                    
-                        
-
-#                    if self.d[f][str(self.df_estoque_all.loc[i, 'Batch'])].get('Stock Amount') == None:
-#                        self.d[f][str(self.df_estoque_all.loc[i, 'Batch'])]['Stock Amount'] = self.df_estoque_all.loc[i, 'Stock'] #number
-#                    else:
-#                        self.d[f][str(self.df_estoque_all.loc[i, 'Batch'])]['Stock Amount'] += self.df_estoque_all.loc[i, 'Stock'] #number
-#                    self.d[f][str(self.df_estoque_all.loc[i, 'Batch'])]['Plant'] = str(self.df_estoque_all.loc[i, 'Plant'])
-#                    self.d[f][str(self.df_estoque_all.loc[i, 'Batch'])]['Batch status key'] = str(self.df_estoque_all.loc[i, 'Batch status key'])
-#
-#                    #Expiration date
-#                    self.d[f][str(self.df_estoque_all.loc[i, 'Batch'])]['Shelf life'] = (self.df_estoque_all.loc[i, 'Expiration date'], self.df_estoque_all.loc[i, 'Expiration date'].strftime('%Y-%m-%d'))
-#                    
-#                    #days (timedelta)
-#                    delta = str(date.today() - self.d[f]['Shelf life'][0].date())
-#                    self.d[f][str(self.df_estoque_all.loc[i, 'Batch'])]['Days'] = eval(delta[:delta.find(' days')])
-#                    self.d[f][str(self.df_estoque_all.loc[i, 'Batch'])]['Month'] = float('{:.1f}'.format(eval(delta[:delta.find(' days')])/30))
-#
-#                    limit = self.d[f][str(self.df_estoque_all.loc[i, 'Batch'])]['Shelf life'][0].date() - timedelta(days=30*12)
-#                    self.d[f][str(self.df_estoque_all.loc[i, 'Batch'])]['Limit sales date'] = (limit, limit.strftime('%Y-%m-%d'))
-#         
-#
-#
-#        for key in list(self.d.keys()):
-#            for i in range(len(self.df_vendas)):
-#                if self.df_vendas.loc[i, 'Material'] == key and self.df_vendas.loc[i, 'Batch'] in list(self.d[key].keys()):
-#                    self.d[key][str(self.df_estoque_all.loc[i, 'Batch'])]['Quantity'] = self.df_vendas.loc[i, 'Quantity']
-#                    #Sales::::
-#                    self.d[key][str(self.df_estoque_all.loc[i, 'Batch'])]['Sales'] = self.d[key].get('Quantity')*(-1) #if self.d[key].get('Quantity') != None and str(self.d[key].get('Quantity')).replace('.', '').isdigit else nan
-#
-#            for i in range(len(self.df_colocado)) and self.df_vendas.loc[i, 'Batch'] in list(self.d[key].keys()):
-#                if self.df_colocado.loc[i, 'Código'] == key:
-#                    self.d[key]['Colocado'] = self.df_colocado.loc[i, 'Colocado']
-#                    try:
-#                        self.d[key]['Delivery'] = self.d[key]['Colocado'] - self.d[key]['Sales']
-#                    except:
-#                        if self.d[key].get('Sales') != None:
-#                            self.d[key]['Delivery'] = self.d[key]['Sales']*(-1)
-
-
+        tables = {}
+        cols = self.df_forecast.columns
+        col_input = 'JAN 2021'
+        for key in list(self.d.keys()):
+            tables[key] = {}
         
+            for i in range(len(self.df_forecast)):
+                if self.df_forecast.loc[i, 'Product Code'] == key:
+                    #print(self.df_forecast.loc[i, col_input])
+                    tables[key]['col_input'] = self.df_forecast.loc[i, col_input]
+
+
+
+
+                    
 
 
 
@@ -154,51 +129,6 @@ class Medicamentos:
 
 x = Medicamentos(sheets)
 x.calcular()
-
-
-
-#for i in range(len(self.df_vendas)):
-#    for key in list(d.keys()):
-#
-#        if self.df_vendas.loc[i, 'Material'] == key:
-#
-#            if self.df_vendas.loc[i, 'Batch'] == self.d[key]['Batch']:
-#                self.d[key]['Quantity'] = str(self.df_vendas.loc[i, 'Quantity']).replace('.', '')
-#
-#    for i in range(len(self.df_colocado)):
-#        for key in list(d.keys()):
-#            if self.df_colocado.loc[i, 'Código'] == key:
-#                self.d[key]['Colocado'] = str(self.df_colocado.loc[i, 'Colocado']).replace('.', '')
-#
-#for key in list(d.keys()):
-#
-#    self.d[key]['Sales'] = eval(self.d[key].get('Quantity'))*(-1) if self.d[key].get('Quantity') != None and str(self.d[key].get('Quantity')).replace('.', '').isdigit else 0
-#    if self.d[key].get('Colocado') != None and self.d[key].get('Colocado').isdigit():
-#        self.d[key]['Delivery'] = eval(self.d[key]['Colocado']) - self.d[key]['Sales']
-#    else:
-#        self.d[key]['Delivery'] = self.d[key]['Sales']*(-1)
-#    self.d[key]['Stock Amount'] = eval(self.d[key]['Stock'])/1000 if self.d[key].get('Stock') != None else '-'
-#
-#    if self.d[key].get('Expiration date') != None:
-#
-#        self.d[key]['Shelf life'] = datetime.strptime(str(self.d[key]['Expiration date']), '%Y-%m-%d')
-#        self.d[key]['Days'] = date.today() - self.d[key]['Shelf life'].date()
-#
-#        self.d[key]['Month'] = int(str(self.d[key]['Days'])[:str(self.d[key]['Days']).find(' days')])/30
-#
-#        self.d[key]['Limit sales date'] = (self.d[key]['Shelf life'] - timedelta(30))
-
-
-
-#print(d)
-
-
-#    if str(f) == str(self.df_vendas.loc[i, 'Material']):
-#        codigo_material = [(self.df_vendas.loc[i, 'Material'], self.df_vendas.loc[i, 'Material description'], self.df_vendas.loc[i, 'Quantity']) for i in range(len(self.df_vendas[['Material']]))]
-#
-#prod = {}
-#for t in codigo_material:
-#    t
 
 
 #print(codigo_material)
